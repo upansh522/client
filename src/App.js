021a0c448369ffd8ebf2539d16f2ce4aa412d11f
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2025-02-19 13:40:32 +0530

    Navbar/ Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+ import { Dashboard } from "@mui/icons-material";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+ import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+ import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "scenes/Layout";
+ import { themeSettings } from "theme";
+
+
 function App() {
+  const mode= useSelector((state)=> state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route element={<Layout/>}>
+          <Route path='/' element={<Navigate to ='/dashboard' replace/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          </Route>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
       
     </div>
   );
